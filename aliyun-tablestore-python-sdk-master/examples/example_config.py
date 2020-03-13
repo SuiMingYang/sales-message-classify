c4c4dc6905002b18ea3d8afeb63fb5ef926498c1
James Sui <2282420654@qq.com>
2020-03-13 12:09:59 +0800

    d

--- aliyun-tablestore-python-sdk-master/examples/example_config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-OTS_ENDPOINT = os.getenv("https://instance.cn-hangzhou.ots.aliyuncs.com")
-OTS_ID = os.getenv("LTAI7YAWiLqJmjtB")
-OTS_SECRET = os.getenv("qTnTQVpmn2dcFmytWcQDRibZcTvusP")
-OTS_INSTANCE = os.getenv("lab-test")
-OTS_STS_TOKEN = os.getenv('OTS_TEST_STS_TOKEN')
